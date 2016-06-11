1e906583888af9d11137b60352fa70d6d23e732b
TheYOSH <theyosh@piratenpartij.nl>
2016-06-11 12:26:21 +0200

    Change thought to comment

--- comments.php
+++ comments.php
@@ -25,7 +25,7 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'ppnl' ),
+				printf( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'ppnl' ),
 					number_format_i18n( get_comments_number() ), get_the_title() );
 			?>
 		</h2>
